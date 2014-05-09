ece18ab3776b98f8f6e9581523c48825612697a3
gameguy999 <game_freak_9937@hotmail.com>
2014-05-08 23:23:35 -0400

    Initial version of the server
    Basic C# server to listen and parse messages to perform commands on MPC

---
+++ MPC Remote Control/Controller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPC_Remote_Control
+{
+    class Controller
+    {
+        const uint WM_KEYDOWN = 0x0100;
+        const int VK_SPACE = 0x20;
+        const int VK_PRIOR = 0x21;
+        const int VK_NEXT = 0x22;
+        const int VK_UP = 0x26;
+        const int VK_DOWN = 0x28;
+        const int VK_DECIMAL = 0x6E;
+
+ 
+ 
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+        
+
+        public void ParseMessage(string command)
+        {
+            switch (command)
+            {
+                case "Play/Pause":
+                    SendKey(VK_SPACE);
+                    break;
+                case "Stop":
+                    SendKey(VK_DECIMAL);
+                    break;
+                case "Volume Up":
+                    SendKey(VK_UP);
+                    break;
+                case "Volume Down":
+                    SendKey(VK_DOWN);
+                    break;
+                case "Next":
+                    SendKey(VK_NEXT);
+                    break;
+                case "Previous":
+                    SendKey(VK_PRIOR);
+                    break;
+            }
+        }
+
+        private void SendKey(int VK_KEY)
+        {
+            Process[] mpc = Process.GetProcessesByName("mpc-hc");
+
+            if (mpc.Length == 0) return;
+            if (mpc[0] != null)
+            {
+                PostMessage(mpc[0].MainWindowHandle, WM_KEYDOWN, VK_KEY, 0);
+            }
+        }
+
+    }
+}
---
+++ MPC Remote Control/Server.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace MPC_Remote_Control
+{
+    class Server
+    {
+        private IPEndPoint serverEndPoint;
+        private TcpListener listener;
+        private bool done = false;
+        Controller controller = new Controller();
+
+        Server()
+        {
+            //Create EndPoint for the Server to listen on
+            serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 23621);
+            //Initialize the TcpListener
+            listener = new TcpListener(serverEndPoint);
+        }
+
+        public void Listen()
+        {
+            //Begin listening
+            listener.Start();
+
+            while (!done)
+            {
+                //Assign the accepted socket
+                Socket client = listener.AcceptSocket();
+                //Create thread for communication
+                Thread communication = new Thread(() => Communication(client));
+                //Start thread
+                communication.Start();
+            }
+
+            //Stop Listening
+            listener.Stop();
+        }
+
+        public void Communication(Socket client)
+        {
+            int bufferLength = 20;
+            byte[] buffer = new byte[bufferLength];
+            string message = string.Empty;
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            int messageLength;
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                try
+                {
+                    //blocking read will wait until a message is sent
+                    client.Receive(buffer);
+                    //convert the byte array we received to a string
+                    message = encoder.GetString(buffer);
+
+                    //loop to find first occurence of a null character
+                    for (messageLength = 0; message[messageLength] != '\0'; messageLength++)
+                    {
+                    }
+
+                    //remove the null characters from the message
+                    message = message.Substring(0, messageLength - 1);
+
+                    if (message.Equals("exit"))
+                    {
+                        exit = true;
+                    }
+                    else
+                    {
+                        controller.ParseMessage(message);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    client.Close();
+                    exit = true;
+                }
+            }
+            if (client != null)
+            {
+                client.Close();
+            }
+
+        }
+
+    }
+}
